02111ada12d6dc8962f7b4c8eeebde1255a6e66f
Markus Persson <markusperssonbiz@gmail.com>
2023-04-22 05:16:39 +0200

    feat(Font): added inter font as default with @next/font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
 import '@/styles/index.css'
 
+import {Inter} from '@next/font/google'
 import * as React from 'react'
 
+const inter = Inter({
+  weight: ['400', '600', '700'],
+  subsets: ['latin'],
+  style: ['normal'],
+  variable: '--font-inter',
+})
+
 export default function App({Component, pageProps}) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${inter.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
